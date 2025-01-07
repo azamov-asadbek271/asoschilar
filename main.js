80522f56e85a8e0364957b5c01ca3ebdfcec7c00
azamov-asadbek271 <azamjonovasa45@gmail.com>
2025-01-07 17:23:01 +0500

    scrool toglrandi

--- main.js
+++ main.js
@@ -8,3 +8,28 @@ openModal.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   modal.classList.remove("open_modal_left");
 });
+
+// Animatsiya uchun
+// Matn elementlarini olish
+const scrollTexts = document.querySelectorAll(".animatsiya");
+
+// Scroll hodisasi bo'yicha funktsiya
+function checkScroll() {
+  const windowHeight = window.innerHeight;
+  scrollTexts.forEach(text => {
+    const textPosition = text.getBoundingClientRect().top; // Ekran bilan nisbatan elementning joylashuvi
+    if (textPosition < windowHeight - 100) { // 100px margin
+      text.classList.add('show-text');
+      text.classList.remove('hide-text');
+    } else {
+      text.classList.add('hide-text');
+      text.classList.remove('show-text');
+    }
+  });
+}
+
+// Scrollni tekshirish
+window.addEventListener('scroll', checkScroll);
+
+// Boshlang'ich holatda scroll tekshiruvi
+checkScroll();
